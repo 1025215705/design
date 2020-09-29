9009e1e37d8d28a3239cfa4a473fbf5707af888a
tanli <1025215705@qq.com>
2020-09-29 16:07:42 +0800

    提交

--- vue-ts/src/practice/class/class2-10.vue
+++ vue-ts/src/practice/class/class2-10.vue
@@ -4,7 +4,7 @@
         <el-collapse-item title="class2: ['1', '2', '3'].map(parseInt); 输出结果"
                           name="1">
             <div>结果: {{['1', '2', '3'].map(parseInt)}} <br>
-                 <el-button @click="class2">查看打印信息</el-button>
+                <el-button @click="class2">查看打印信息</el-button>
             </div>
         </el-collapse-item>
         <el-collapse-item title="什么是防抖和节流？有什么区别？如何实现？"
@@ -12,23 +12,29 @@
             <div>
                 防抖：高频率的触发某个事件， 如果在n秒再次被触发， 则重新计算
                 <br>
-                 <el-button @click="scroll">查看效果</el-button>
-               
+                <el-button @click="scroll">查看效果</el-button>
+
             </div>
             <div>
                 节流： 高频率的触发某个事件下， 稀释掉频率
                 <br>
                 <el-button @click="resize">查看效果</el-button>
-               
+
             </div>
         </el-collapse-item>
-        <el-collapse-item title="效率 Efficiency"
+        <el-collapse-item title="介绍下 Set、Map、WeakSet 和 WeakMap 的区别？"
                           name="3">
-            <div>简化流程：设计简洁直观的操作流程；</div>
-            <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-            <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+            <div>set 是一种集合的数据结构， map 是以一种字典的数据结构</div>
+            <div>set 的详细的解析： 1：唯一不重复的， 没有顺序
+                <br>
+                <el-button @click="set">查看效果</el-button>
+            </div>
+            <div>map 的详细解析
+                <br>
+                <el-button @click="map">查看效果</el-button>
+            </div>
         </el-collapse-item>
-        <el-collapse-item title="可控 Controllability"
+        <el-collapse-item title="请分别介绍它们之间的区别和优劣(Object.prototype.toString.call() 、 instanceof 以及 Array.isArray())"
                           name="4">
             <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
             <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
@@ -44,8 +50,8 @@ export default {
     };
   },
   methods: {	
-      handleChange(){
-
+      handleChange(val) {
+        console.log(val);
       },
     class2(){
           console.log("class2");
@@ -79,7 +85,7 @@ debounce(fn) {
     return function () {
         if (timeOut) {
             clearTimeout(timeOut);
-        };
+        }
         timeOut = setTimeout(()=> {
             fn.apply(this)
         }, 1000);
@@ -107,9 +113,142 @@ window.addEventListener('scroll', this.debounce(this.sayHi));
 },
               resize(){
                      window.addEventListener('resize', this.throttle(this.sayHi));
-                   }
+                   },
+                   set(){
+//demo
+const s = new Set();
+[1,2,3,4,3,2,1].forEach(x=>s.add(x));
+for(let i of s){
+    console.log(i);
+}
+console.log("set 的长度", s.size);
+// 去重数组里的重复的对象
+let arr = [1,2,2,33,3,3,1,1];
+console.log([...new Set(arr)]);
+//Set 对象允许你储存任何类型的唯一值，无论是原始值或者是对象引用。
+//向 Set 加入值的时候，不会发生类型转换，所以5和"5"是两个不同的值。
+//Set 内部判断两个值是否不同，使用的算法叫做“Same-value-zero equality”，
+//它类似于精确相等运算符（===），主要的区别是**NaN等于自身，
+//而精确相等运算符认为NaN不等于自身
+
+//set的实例方法
+ let set1 = new Set();
+ console.log(set1.add(1).add(2).add(1));
+ console.log(set1.has(1));
+ console.log(set1.has(3));
+ console.log(set1.delete(1));
+ console.log(set1.has(1));
+ set1.clear()//清空
+ console.log(set1);
+// set 转 数组
+  const items = new Set([1,2,3,4]);
+  const array = Array.from(items);
+  console.log(array);
+  //或
+  const arr1 = [... items];
+  console.log(arr1);
+  //遍历方法
+  let set = new Set([1,2,3]);
+  console.log("keys",set.keys);
+  console.log("values",set.values);
+  console.log("entries",set.entries);
+  console.log("keys")
+  for(let item of set.keys()){
+      console.log(item);
+  }
+  console.log("values")
+  for(let item of set.values()){
+       console.log(item);
+  }
+   console.log("entries")
+ for(let item of set.entries()){
+     console.log(item);
+ }
+ set.forEach((value,key)=>{
+     console.log(key+':'+value)
+ })
+ //set 可以使用map 和 filter 方法
+  let sett = new Set([1,2,3])
+  sett = new Set([...sett].map(item=>item*2));
+  console.log(sett);
+  sett = new Set(...[sett].filter(item=>item>=4));
+  console.log(sett); 
+},
+
+
+
+map(){
+  const m = new Map();
+  const o = {p:'haha'};
+  m.set(o,'content');
+  console.log(m.get(o));
+  console.log(m.has(o));
+  console.log(m.delete(o));
+  console.log(m.has(o));
+  //任何具有 Iterator 接口、且每个成员都是一个双元素的数组的数据结构都可以当作Map构造函数的参数
+  const set = new Set([
+      ['foot',1],
+      ['bar',2]
+  ]);
+  const m1 = new Map(set);
+  m1.get('foot');
+  console.log(m1.get('foot'));
+  const m2 = new Map([['bar', 3]]);
+  const m3 = new Map(m2);
+  m3.get('baz');
+  
+  //注意，只有对同一个对象的引用，Map 结构才将其视为同一个键。这一点要非常小心。
+  let m4 = new Map();
+  m4.set(['a'],555);
+  console.log(m4.get(['a'])); 
+  //map 的属性和方法
+  const map = new Map([
+      ['name','An'],
+      ['des','JS']
+  ]);
+  console.log("map 的长度",map.size);
+  console.log(map.entries());
+  console.log(map.keys());
+  // map 转 Array
+  const m6 = new Map([[1,1],[2,2],[3,3]]);
+  console.log([...map]);
+  // Array 转 map
+  const m7 = new Map([[1,1],[2,2],[3,3]]);
+  console.log(m7);
+  //map 转 object
+  function mapToObj(map){
+    let o = {};
+    map.forEach(ele=>{
+        o[ele.key] = ele.value;
+    });
+    return o;
+  }
+ const mapp = new Map().set('name','An').set('des','js');
+ console.log(mapToObj(mapp));
+ //object 转 map
+  function objToMap(obj){
+     let map = new Map();
+     for(let key of Object.keys(obj)){
+         map.set(key,obj[key]);
+     }
+     return map;
+  }
+  console.log(objToMap({'name':'An','des':'Js'}));
+  //Map 转 json 
+  function mapToJson(map){
+      return JSON.stringify([...map]);
+  }
+  let m8 = new Map().set('name','An').set('des','js');
+  console.log(mapToJson(m8));
+  //json 转 map
+  function jsonToStrMap(jsonStr){
+      return objToMap(JSON.parse(jsonStr));
+  }
+  console.log(jsonToStrMap('{"name":"An","des":"JS"}'))
+}
 
 }
+
 }
 </script>
 <style>
